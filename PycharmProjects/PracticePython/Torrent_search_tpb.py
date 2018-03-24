f16589fb1d8ab8586484c413a02a136526576efe
Mario <mariobluemind@gmail.com>
2018-03-24 19:57:14 +0100

    top3magnet(search) to print top 3 results on screen

--- PycharmProjects/PracticePython/Torrent_search_tpb.py
+++ PycharmProjects/PracticePython/Torrent_search_tpb.py
@@ -11,22 +11,47 @@ def search_builder(nam):
 def check_search(built_url):
     pag_req_raw = requests.get(url=built_url)
     pag_req = pag_req_raw.text
+    magnets = []
+    rawtitles = []
     if "No hits" in pag_req:
         print("No hits")
     else:
         soup = BeautifulSoup(pag_req,"html.parser")
         hrefs = soup.find_all('a', href=True)
-        for ref in hrefs:
-            print(ref)
-        print(hrefs)
+        titles = soup.find_all(class_="detName")
+        for href in hrefs:
+            hrf = str(href.get('href'))
+            if hrf.startswith("magnet"):
+                magnets.append(hrf)
+
+        for tit in titles:
+            rawtitles.append(tit.getText())
+
+
+        for i in range(3):
+            print("{0}\n {1}\n".format(rawtitles.pop(),magnets.pop()))
 
 
 
-def magnet(mgn):
+
+
+
+def open_magnet(mgn):
     ssoo = sys.platform
     if ssoo.startswith('win32'):
         os.startfile(mgn)
     if ssoo.startswith('linux'):
-        subprocess.Popen(['xdg-open', magnet],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.Popen(['xdg-open', mgn],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-magnet(strin)
+#TODO String formating for SXXEYY s: season e:episode and HD/720...
+def top3magnet(busqueda):
+    if busqueda is '':
+        print("Busqueda vacía")
+    else:
+        url = search_builder(busqueda)
+        check_search(url)
+
+
+
+if __name__ == '__main__':
+    top3magnet("Game of Thrones")
